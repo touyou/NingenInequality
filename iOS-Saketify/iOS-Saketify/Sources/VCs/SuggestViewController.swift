0c585a8b0e60c22c187f6e20d67cd74736b58f37
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-05 15:23:14 +0900

    installed Koloda

---
+++ iOS-Saketify/iOS-Saketify/Sources/VCs/SuggestViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SuggestViewController.swift
+//  iOS-Saketify
+//
+//  Created by Satoshi Komatsu on 2019/10/05.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+final class SuggestViewController: UIViewController {
+    
+}
