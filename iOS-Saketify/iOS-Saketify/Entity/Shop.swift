fdfea75e70efdd5ee96d520489cdde8e2001d7fe
touyou <fujiyou1121@gmail.com>
2019-10-05 12:42:23 +0900

    add shop base

---
+++ iOS-Saketify/iOS-Saketify/Entity/Shop.swift
@@ -0,0 +1,15 @@
+//
+//  Shop.swift
+//  iOS-Saketify
+//
+//  Created by 藤井陽介 on 2019/10/05.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+
+struct Shop: Codable {
+    var x: Double
+    var y: Double
+    var name: String
+}
