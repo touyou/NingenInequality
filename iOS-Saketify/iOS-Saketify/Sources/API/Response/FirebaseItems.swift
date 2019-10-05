06711721e6574e0a28882a5cb35c41436bb3273d
touyou <fujiyou1121@gmail.com>
2019-10-05 15:06:39 +0900

    add firebase code base

---
+++ iOS-Saketify/iOS-Saketify/Sources/API/Response/FirebaseItems.swift
@@ -0,0 +1,15 @@
+//
+//  FirebaseItems.swift
+//  iOS-Saketify
+//
+//  Created by 藤井陽介 on 2019/10/05.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+
+struct Sake: Codable {
+    var id: UInt
+    var name: String
+    var rating: Double
+}
