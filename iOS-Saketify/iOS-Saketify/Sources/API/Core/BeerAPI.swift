fbefc76a3d27066a09f27df10c65b0b4ff935ede
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-05 14:50:02 +0900

    installed Firebase

---
+++ iOS-Saketify/iOS-Saketify/Sources/API/Core/BeerAPI.swift
@@ -0,0 +1,36 @@
+//
+//  BeerAPI.swift
+//  iOS-Saketify
+//
+//  Created by Satoshi Komatsu on 2019/10/05.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import Foundation
+import APIKit
+
+final class BeerAPI {
+    private init() {}
+    
+    struct SuggestBeer: BeerRequest {
+        let longitude: String
+        let latitude: String
+        
+        typealias Response = SuggestBeerRequest
+        
+        var method: HTTPMethod {
+            return .get
+        }
+        
+        var path: String {
+            return "/default/beer1005-1"
+        }
+        
+        var parameters: Any? {
+            return [
+                "x": longitude,
+                "y": latitude
+            ]
+        }
+    }
+}
