ce52abb25016e4fd48780fde3fcee2ffaef3dad4
tosh7 <zlia.6.lj.425@gmail.com>
2019-10-05 12:12:43 +0900

    regenerated iOS file

---
+++ iOS-Saketify/iOS-Saketify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iOS-Saketify
+//
+//  Created by Satoshi Komatsu on 2019/10/05.
+//  Copyright © 2019 Satoshi Komatsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
